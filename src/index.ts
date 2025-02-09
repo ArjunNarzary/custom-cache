aaea7279c9e3c05d6b2fbb0c0c56a899b8e03583
Arjun Narzary <arjunn@byteridge.com>
2025-02-09 11:29:17 +0530

    Project init

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from "express"
+
+require("dotenv").config()
+
+const app = express()
+
+const PORT = process.env.PORT || 8000
+
+app.use(express.json())
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
